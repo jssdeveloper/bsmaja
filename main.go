aaebf74de6857f7f02f4f70159c4ae70449bb7cc
Janis Stals <janisstals@Janiss-Mac-mini.local>
2023-12-27 16:19:52 +0200

    added section delivery

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 		return c.Render("base", fiber.Map{})
 	})
 
-	log.Fatal(app.Listen("192.168.0.180:3100")) // dev
+	log.Fatal(app.Listen("192.168.0.164:3100")) // dev mac mini
+	// log.Fatal(app.Listen("192.168.0.180:3100")) // dev macbook
 	// log.Fatal(app.Listen(":80")) // prod
 }
